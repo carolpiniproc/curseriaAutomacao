9c908f2f87c0b0ef0cee26098d717add34eb9a76
Carolina Procaccini <cprocaccini@ciandt.com>
2020-07-15 23:20:54 -0300

    fixed a test : changed ID by CSSselector and wrote on Readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # curseriaAutomacao
-Curso de Automacao Java + Cucumber + Junit
+Curso de Automacao Java + Selenium + Junit + Cucumber
+
+## Requerimentos  
+1 - JDK > 1.8  
+2 - Maven project  
+3 - ChromeDriver de acordo com a versão do seu Chrome  
+
+---
+
+
+
+
+
+
+
--- src/main/Mappers/InicialMap.java
+++ src/main/Mappers/InicialMap.java
@@ -2,7 +2,7 @@ public class InicialMap {
 
     Element pesquisa = new Element(ByValue.ID,"search");
     Element tituloLivro = new Element(ByValue.XPATH,"//h2/a");
-    Element preco = new Element(ByValue.ID,"#product-price-44 > span");
+    Element preco = new Element(ByValue.CSS,"#product-price-44 > span");
     Element livros = new Element(ByValue.CSS,"ul.products-grid > li");
     Element precoLista = new Element(ByValue.CSS,"span.price");
     Element tituloLivroLista = new Element(ByValue.CSS,"h2 > a");
